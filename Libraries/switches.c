5e368d1f1aad89c3b36383dbabab8181d6735e54
John P Doldol <jdoldol1@nait.ca>
2018-01-30 10:21:16 -0700

    update libraries

---
+++ Libraries/switches.c
@@ -0,0 +1,85 @@
+#include "derivative.h"
+#include "Delay_C.h"
+
+/*
+;________________________________________________________
+;                                                        |
+;    Library:  Switches_C.c                              |
+;                                                        |
+;    Contains:                                           |
+;              Sw_Init(void)                             |
+;              Get_Switches()                            |
+;              Wait_for_Switches_up(void)                |
+;                                                        |
+;    Author:   John Doldol                               |            |
+;                                                        |
+;    Last Modified: Jan 29, 2018 (Created)               |
+;________________________________________________________|
+
+;________________________________________________________
+;                                                        |
+;    Subroutines:   Sw_Init()                            |
+;                                                        |
+;    Requires:      None                                 |
+;    Returns:       None                                 |
+;________________________________________________________|
+
+*/
+
+void Sw_Init(void)
+{
+  ATD1DIEN1 |= 0x1F;  
+}
+
+/*
+;________________________________________________________
+;                                                        |
+;    Subroutines:   Get_Switches(void)                   |
+;                                                        |
+;    Requires:      None                                 |
+;    Returns:       char - the down switches             |
+;________________________________________________________|                           
+*/
+
+unsigned char Get_Switches(void)
+{
+  unsigned char Switches = 0;
+  Switches = PT1AD1 & 0x1F;
+  Delay_C(10);
+  
+  if((PT1AD1 & 0x1F) != Switches)
+    Switches = 0;
+  
+  return Switches;
+}
+
+/*
+;________________________________________________________
+;                                                        |
+;    Subroutines:   Wait_for_Switches_up(void)           |
+;                                                        |
+;    Requires:      None                                 |
+;    Returns:       None                                 |
+;________________________________________________________|
+*/
+
+void Wait_for_Switches_up(void)
+{
+  while(PT1AD1 & 0x1F){}
+}
+
+/*
+;________________________________________________________
+;                                                        |
+;    Subroutines:                                        |
+;          Wait_for_Switches_up_mask(unsigned char mask) |
+;                                                        |
+;    Requires:      None                                 |
+;    Returns:       None                                 |
+;________________________________________________________|
+*/
+
+void Wait_for_Switches_up_mask(unsigned char mask)
+{
+  while(PT1AD1 & mask){}
+}                 
---
+++ Libraries/switches.h
@@ -0,0 +1,4 @@
+void Sw_Init(void);
+unsigned char Get_Switches(void);
+void Wait_for_Switches_up(void);
+void Wait_for_Switches_up_mask(unsigned char mask);
