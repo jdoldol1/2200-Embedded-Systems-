b6d6a7efe98fd8f90a23602a2b2b4ba3e4f90185
John P Doldol <jdoldol1@nait.ca>
2018-02-01 13:41:48 -0700

    Need to finishedh top/bot methods

--- Libraries/led_7seg.c
+++ Libraries/led_7seg.c
@@ -106,7 +106,123 @@ void Char_7Seg_C(unsigned char pos, unsigned char c)
 ;       Details: Sends character in b to position in A 
 ;________________________________________________________
 */
-void Char_7Seg_C_D(unsigned char Pos, unsigned char character) {}
+void Char_7Seg_C_D(unsigned char Pos, unsigned char character) 
+{
+	Pos |= 0x05;
+	PORTB = Pos;
+
+	PORTA = 0x02;
+	PORTA = 0x03;
+
+	PORTB = character;
+
+	PORTA = 0x00;
+	PORTA = 0x01;
+}
+
+/*
+;________________________________________________________
+;
+;             Char_7Seg_C
+;
+;       Requires:     Pos has position 
+;       Returns:        n/a
+;       Regs Affected:  n/a
+;
+;       Details: Clears all segments of char in position A
+;________________________________________________________
+*/
+
+void Clear_7Seg(unsigned char Pos)
+{
+	Pos |= 0x07;
+	PORTB = Pos;
+
+	PORTA = 0x02;
+	PORTA = 0x02;
+
+	PORTB = 0x08;
+
+	PORTA = 0x00;
+	PORTA = 0x01;
+}
+
+/*
+;________________________________________________________
+;
+;             LEDS_7Seg_Init_C
+;
+;       Requires:		None
+;
+;       Returns:        None
+;       Regs Affected:  None
+;
+;       Details: This is a set of instructions to clear 
+;		the memory of the 7 segment LED digits - 
+;		used prior to writing anything to them
+;________________________________________________________
+*/
+
+void LEDS_7Seg_Init_C(void)
+{
+	signed char i = 0;
+	unsigned char Digit = 0;
+	DDRA = 0x03;
+	DDRB = 0xff;
+	PORTA = 0x03;
+
+	for (i = 8; i >= 0; i--)
+	{
+		Digit = (0x70 | i);
+		PORTB = Digit;
+
+		PORTA = 0b00000010;
+		PORTA = 0b00000011;
+
+		PORTB = 0b10000000;
+		PORTA = 0b00000000;
+		PORTA = 0b00000001;
+		Digit++;
+	}
+}
+
+/*
+;________________________________________________________
+;
+;             Top_8Out_C
+;
+;       Requires:		unsigned char input parameter
+;
+;       Returns:        n/a
+;       Regs Affected:  n/a
+;
+;       Details: This routine places input data on the 
+;		top row of the 7 segment LEDS, 
+		;upper two digits of row are blanked
+;________________________________________________________
+*/
+void Top_8Out_C(unsigned char pos)
+{
+
+}
+
+/*
+;________________________________________________________
+;
+;             Top_16Out_C
+;
+;       Requires:		16 bit data is in iIn
+;
+;       Returns:        n/a
+;       Regs Affected:  n/a
+;
+;       Details: This routine places 16 bit data contained 
+;		iIn on the top row of the 7 segment LEDS
+;________________________________________________________
+*/
+
+
+
 
 
 
---
+++ Libraries/led_7seg.h
@@ -0,0 +1,8 @@
+void Char_7Seg_C(unsigned char pos, unsigned char c);
+void LEDS_7Seg_Init_C(void);
+void Clear_7Seg(unsigned char Pos);
+void Char_7Seg_C_D(unsigned char Pos, unsigned char character);
+void Bot_8Out_C(unsigned char c);
+void Top_8Out_C(unsigned char pos);
+void Bot_16Out(unsigned int iIn);
+void Top_16Out(unsigned int iIn);
