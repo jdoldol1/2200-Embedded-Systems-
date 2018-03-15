487fb26841f09ad603ac169d2ea2c67feb8f204a
John P Doldol <jdoldol1@nait.ca>
2018-03-15 13:48:50 -0600

    --close to working

--- ICA11 JohnDoldol/Sources/main.c
+++ ICA11 JohnDoldol/Sources/main.c
@@ -19,16 +19,31 @@
 /********************************************************************/
  #include "LCD_C.h"
  #include "SCI_Lib_C.h"
+ #include "my_delays.h"
+ #include "led_7seg.h"
 
 /********************************************************************/
 //		Prototypes
 /********************************************************************/
 
-
+ void GetValue(void);
+ 
 /********************************************************************/
 //		Variables
 /********************************************************************/
-
+ float x = 0;
+ float y = 0;
+ char DestString[25] = "";
+ char SrcString[2] = "";
+ char RxData;
+ 
+ char ClearAndHome[] = "\x1B[2J\x1B[H";
+ char StartXValue[] = "\x1b[2;10H";
+ char StartYValue[] = "\x1b[5;5H";
+ char promptx[] = "Enter first value: ";
+ char prompty[] = "Enter second value: ";
+ char gotit[] = "Got it!";
+ char result[]="";
 
 /********************************************************************/
 //		Lookups
@@ -39,8 +54,29 @@ void main()
 {
   _DISABLE_COP();
   SCI0_Init19200();
-  {    
+  LEDS_7Seg_Init_C();
+  
+  SCI0_TxString(ClearAndHome);
+  SCI0_TxString(promptx);
+  for(;;)
+  { 
+  
+    GetValue();    
+    x = atof(DestString);   
        
+    Delay_C(5000);   
+    
+    SCI0_TxString(gotit);
+    SCI0_TxString(DestString);
+    SCI0_TxString(StartXValue);
+    SCI0_TxString(prompty);
+    
+    GetValue();
+    y = atof(DestString);
+    Delay_C(5000);
+    
+    SCI0_TxString(StartYValue);
+    SCI0_TxString(DestString);
       
   } /* loop forever */
   /* please make sure that you never leave main */
@@ -49,7 +85,18 @@ void main()
 /********************************************************************/
 //		Functions
 /********************************************************************/
-
+void GetValue(void)
+{
+  do
+    {
+    
+      RxData = SCI0_RxByte();
+      SCI0_TxByte(RxData);
+      SrcString[0] = RxData;
+      (void)strcat(DestString,SrcString);
+    
+    }while(RxData != 0x0d);
+}
 /********************************************************************/
 //		Interrupt Service Routines
 /********************************************************************/
--- Libraries/SCI_Lib_C.h
+++ Libraries/SCI_Lib_C.h
@@ -1,5 +1,5 @@
 unsigned char SCI0_Key_hit(void);
 void SCI0_Init19200(void);
 void SCI0_TxByte(unsigned char);
-unsigned char SCI0_TxByte(void);
+unsigned char SCI0_RxByte(void);
 void SCI0_TxString(char *);
